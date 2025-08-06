355e92dac1ffc6a0493bfafc42add9c130609aeb
Abdulazeez Adam <abdulazeezadam09@email.com>
2025-08-06 10:55:34 +0100

    tyjhytjyt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import RouteChangeTracker from './components/RouteChangeTracker';
 
 // Layouts
